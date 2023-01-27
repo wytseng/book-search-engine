65ffde2204b2f749de509b7a4a44484267eeb7cb
Steph T <wytseng@uci.edu>
2023-01-27 14:16:07 -0800

    set up app.js to route with apollo client

--- server/server.js
+++ server/server.js
@@ -33,7 +33,7 @@ const startApolloServer = async(typeDefs, resolvers) => {
 
   db.once('open', () => {
     app.listen(PORT, () => {
-      console.log(`🌍 Now listening on localhost:${PORT}!`);
+      console.log(`🌍 API server running on port ${PORT}!`);
       console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
     });
   });
